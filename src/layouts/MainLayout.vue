6ef80490a6cb4ff63287c83fc04f275414e77fc1
sparra-sirt <sparra@sirt.com>
2021-04-08 17:42:29 +0200

    First commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,106 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+      </q-toolbar>
+      <div class="q-px-lg q-pt-xl q-mb-md">
+        <div class="text-h3">Todo</div>
+        <div class="text-subtitle1">{{ todaysDate }}</div>
+      </div>
+      <q-img src="../assets/mountains.png" class="header-image absolute-top" />
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      :width="250"
+      :breakpoint="600"
+    >
+      <q-scroll-area
+        style="
+          height: calc(100% - 192px);
+          margin-top: 192px;
+          border-right: 1px solid #ddd;
+        "
+      >
+        <q-list padding>
+          <q-item clickable v-ripple to="/" exact>
+            <q-item-section avatar>
+              <q-icon name="list" />
+            </q-item-section>
+
+            <q-item-section> Todo </q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple to="/help" exact>
+            <q-item-section avatar>
+              <q-icon name="help" />
+            </q-item-section>
+
+            <q-item-section> Help </q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
+
+      <q-img
+        class="absolute-top"
+        src="https://cdn.quasar.dev/img/material.png"
+        style="height: 192px"
+      >
+        <div class="absolute-bottom bg-transparent">
+          <q-avatar size="56px" class="q-mb-sm">
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+          </q-avatar>
+          <div class="text-weight-bold">Sergio Parra</div>
+          <div>@sergioParra</div>
+        </div>
+      </q-img>
+    </q-drawer>
+
+    <q-page-container>
+      <keep-alive>
+        <router-view />
+      </keep-alive>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from "components/EssentialLink.vue";
+import { date } from "quasar";
+
+export default {
+  name: "MainLayout",
+  components: {
+    EssentialLink,
+  },
+  data() {
+    return {
+      leftDrawerOpen: false,
+    };
+  },
+  computed: {
+    todaysDate() {
+      let timeStamp = Date.now();
+      return date.formatDate(timeStamp, "dddd D MMMM");
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.header-image {
+  height: 100%;
+  z-index: -1;
+  opacity: 0.2;
+  filter: grayscale(100%);
+}
+</style>
---
+++ src/pages/Help.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="q-pa-lg">
+  <h5 class="q-mt-none">Help</h5>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ratione incidunt ad quas voluptas consequuntur, porro iure aut laborum nisi facilis numquam possimus quae, veritatis nemo distinctio aliquam deserunt explicabo.</p>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non neque eum dolore debitis repudiandae libero deserunt consectetur. Esse pariatur expedita id nulla, nobis veniam at minima dolorum necessitatibus debitis ducimus?</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
